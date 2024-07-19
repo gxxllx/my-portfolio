7340ebac58753a40dc7bb8243b771a649b9403b3
Guillermo Comin <guillecom04@gmail.com>
2024-07-19 10:15:54 +0200

    logros

---
+++ src/components/AchievementsSection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const achievementsList = [
+  {
+    metric: "Projects",
+    value: "5+",
+  },
+  {
+    metric: "Commits",
+    value: "50+",
+  },
+  {
+    metric: "Years",
+    value: "1+",
+  },
+];
+
+const AchievementsSection = () => {
+  return <div></div>;
+};
+
+export default AchievementsSection;
