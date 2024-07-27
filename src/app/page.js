3867a906cbabf23dc45545f5dce3a5ce3f12d7e6
Guillermo Comin <guillecom04@gmail.com>
2024-07-27 13:18:54 +0200

    arreglo movil

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,11 @@ import ProjectsSection from "@/components/ProjectsSection";
 import EmailSection from "@/components/EmailSection";
 import Footer from "@/components/Footer";
 import AchievementsSection from "@/components/AchievementsSection";
+import "@/styles/globals.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-[#121212] mx-auto px-6 fix:px-12 py-4">
       <NavBar />
       <div className="container mt-24 mx-auto px-1 sm:px-0 py-4">
         <HeroSection />
